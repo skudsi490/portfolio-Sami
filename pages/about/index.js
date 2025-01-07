d2fa1dd150b863a1eb8ed87b00271b462c875454
Sami Kudsi <skudsi490@gmail.com>
2025-01-07 22:34:09 +0100

    correction

--- pages/about/index.js
+++ pages/about/index.js
@@ -179,7 +179,7 @@ const About = () => {
               {/* clients */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={4} duration={5} />
+                  <CountUp start={0} end={1} duration={5} />
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   {" "}
